ba792162419fcf8aef0d622ebcec98a3b01d9732
OZapotylok <olga.zapotylok.98@mail.ru>
2018-09-14 08:48:46 +0300

    add a simple console calculator program

---
+++ Calculator/src/com/epam/Main.java
@@ -0,0 +1,48 @@
+package com.epam;
+
+import java.util.Scanner;
+
+/**
+ * Created by olga.zapotylok on 12.09.2018 in 01:11.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please, enter the first number: ");
+        double firstNumber = scanner.nextDouble();
+        System.out.print("Please, enter the second number: ");
+        double secondNumber = scanner.nextDouble();
+
+        for (; ; ) {
+            System.out.println(
+                    "Select operation:"
+                            + " +, -, *, / (enter 'exit' to exit)");
+            String operation = scanner.next();
+
+            if (operation.equals("exit")) {
+                break;
+            }
+            System.out.println(calculate(firstNumber, secondNumber, operation));
+        }
+    }
+
+    
+    public static String calculate(double fNumber, double sNumber, String operation) {
+        String result = "Result is: ";
+        if (operation.equals("+")) {
+            result += fNumber + sNumber;
+        } else if (operation.equals("-")) {
+            result += fNumber - sNumber;
+        } else if (operation.equals("*")) {
+            result += fNumber * sNumber;
+        } else if (operation.equals("/")) {
+            result += sNumber != 0 ? fNumber / sNumber : "Dividing by zero is undefined";
+        } else {
+            result += "invalid data, try again";
+        }
+        return result;
+
+    }
+}
